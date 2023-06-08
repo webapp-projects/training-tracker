f67bb7aca51365da91fe6deea10142b732d8bd5b
adrian <agrski06@gmail.com>
2023-06-08 13:59:39 +0200

    Add basic auth

--- server/server.js
+++ server/server.js
@@ -1,4 +1,25 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 8080;
-app.listen(port, () => console.log(`Nasłuchiwanie na porcie ${port}`));
+require('dotenv').config()
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const connection = require('./db')
+const userRoutes = require("./routes/users")
+const authRoutes = require("./routes/auth")
+
+//open connection with database
+connection()
+
+//middleware
+app.use(express.json())
+app.use(cors())
+const port = process.env.PORT || 8080
+
+// routes
+app.use("/api/users", userRoutes)
+app.use("/api/auth", authRoutes)
+
+app.get("/", (req, res) => {
+    res.send("TEST")
+})
+
+app.listen(port, () => console.log(`Listening on port: ${port}, http://localhost:${port}`))
